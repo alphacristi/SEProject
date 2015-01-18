f32b96daffbcfe9082d12249abb4d1da8357da28
Cristian <alpha.cristi@yahoo.com>
2015-01-19 00:45:28 +0200

    finished Handshake class

--- src/mta/se/proiectchat/mainpackage/cryptopackage/Handshake.java
+++ src/mta/se/proiectchat/mainpackage/cryptopackage/Handshake.java
@@ -1,6 +1,14 @@
 package mta.se.proiectchat.mainpackage.cryptopackage;
 
+import mta.se.proiectchat.mainpackage.tcpconnctionpackage.CallerConnection;
 import mta.se.proiectchat.mainpackage.tcpconnctionpackage.ISocketConnection;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.security.*;
+import java.security.cert.X509Certificate;
 
 /**
  * This is the class used for establishing an aes key
@@ -16,22 +24,90 @@ public class Handshake {
 
 
     /**
-     * This function returns an aes key for a secure comunication
+     * * This function starts a handshake and returns an aes key for a secure comunication
+     * the socket conection must be opened !!!
      *
-     * @param certificateFilename - filename of caller certificate
-     * @param destination         - a socket connection to destination
-     * @return - a byte[] with a aes key
+     * @param alias               - alias for stored priv key in JKS
+     * @param password            - password for stored priv key in JKS
+     * @param certificateFilename - filename of certificate
+     * @param destination         - connection to caller
+     * @return
      */
-    public byte[] establishAESkey(String certificateFilename, ISocketConnection destination) {
+    public static byte[] establishAESkey(String alias, String password, String certificateFilename, ISocketConnection destination) {
 
-        byte[] aeskey = new byte[16];
-
-
-
-        destination.Open();
+        byte[] aeskey = null;
+        byte[] certFromFile = CertificateManager.readCertificateFromFile(certificateFilename);
 
 
-        destination.Close();
+        destination.Write(certFromFile); // sending certificate
+
+        byte[] encAESkey = destination.Read();
+        PrivateKey privateKey = CertificateManager.LoadPrivKey(alias, password);
+        try {
+            Cipher RSAcipher = Cipher.getInstance("RSA");
+            RSAcipher.init(Cipher.DECRYPT_MODE, privateKey);
+            aeskey = RSAcipher.doFinal(encAESkey);
+
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        }
+
+
         return aeskey;
     }
+
+
+    /**
+     * This method waits for a handshake from caller
+     *
+     * @param destination - socket connection of caller
+     * @return - a byte array of established aes key
+     */
+    public static byte[] waitForHandshake(ISocketConnection destination) {
+
+        byte[] _certificate = null;
+        _certificate = destination.Read();
+
+        X509Certificate certificate = CertificateManager.loadFromByteArray(_certificate);
+        PublicKey pubkey = certificate.getPublicKey();
+        SecureRandom sRandom = new SecureRandom();
+        byte[] aeskey = new byte[16];
+        sRandom.nextBytes(aeskey);
+
+        byte[] encAESkey = null;
+        Cipher RSAcipher = null;
+        try {
+            RSAcipher = Cipher.getInstance("RSA");
+            RSAcipher.init(Cipher.ENCRYPT_MODE, pubkey);
+            encAESkey = RSAcipher.doFinal(aeskey);
+
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        }
+
+        destination.Write(encAESkey);
+
+
+        return aeskey;
+    }
+
+
 }
