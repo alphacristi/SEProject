769176682805413b0299a7f20efdcc549bb9b3d5
adriansnep <snep_adi2000@yahoo.com>
2015-01-25 13:04:55 +0200

    added test function for recording data

--- src/mta/se/proiectchat/mainpackage/Main.java
+++ src/mta/se/proiectchat/mainpackage/Main.java
@@ -1,5 +1,7 @@
 package mta.se.proiectchat.mainpackage;
 
+import mta.se.proiectchat.mainpackage.audiopackage.Records;
+
 /**
  * This class which conaints main function - the entry point in program
  * Created by Cristian on 1/18/2015.
@@ -10,6 +12,27 @@ public class Main {
      */
     public static void main(String arvs[]) {
 
+       try {
+           Records client = new Records();
+           byte[] messageToSend=readMessage(client);
+       }
+       catch (Exception e) {
+           e.printStackTrace();
+       }
 
+
+    }
+
+
+    public static byte[] readMessage(Records client){
+        try {
+            client.openPickupLine();
+            byte[] dataCaptured = client.captureAudioData();
+            return dataCaptured;
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
