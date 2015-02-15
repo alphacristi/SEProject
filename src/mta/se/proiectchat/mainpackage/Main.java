e45f8e0be707251582ab49b5292b29112f53f32f
Cristian <alpha.cristi@yahoo.com>
2015-02-15 20:26:31 +0200

    finished main class which is the entry point in application

--- src/mta/se/proiectchat/mainpackage/Main.java
+++ src/mta/se/proiectchat/mainpackage/Main.java
@@ -4,6 +4,12 @@ import mta.se.proiectchat.mainpackage.audiopackage.Playback;
 import mta.se.proiectchat.mainpackage.audiopackage.Records;
 import mta.se.proiectchat.mainpackage.tcpconnctionpackage.CallerConnection;
 
+import java.awt.*;
+import java.io.Console;
+import java.io.IOException;
+import java.time.chrono.MinguoChronology;
+import java.util.logging.ConsoleHandler;
+
 /**
  * This class which conaints main function - the entry point in program
  * Created by Cristian on 1/18/2015.
@@ -12,67 +18,105 @@ public class Main {
     /**
      * This is the entry point in program
      */
-    public static void main(String arvs[]) {
+    public static void main(String args[]) {
 
-        try {
-            Records client = new Records();
-            byte[] messageToSend = readMessage(client);
+        // Setup.setup("cristi","password"); // pentru generarea javakeytool si certificat
+        String username = "";
+        String password = "";
 
-            // sendData(messageToSend);
+        if (args.length == 0) {
+            System.out.println("Please enter the username :");
+            username = Main.getUsername();
+            System.out.println("Please enter the password :");
+            password = Main.getPassword();
 
-            //   byte[] messageReceived=receiveData();
+            //TODO : in asteptarea unui apel, instantierea clasei pentru apeluri
 
-            Playback client2 = new Playback();
-            playMessage(client2, messageToSend);
-        } catch (Exception e) {
-            e.printStackTrace();
+
+        } else {
+
+            if (args[0].equals("-setup")) {
+                System.out.println("Please enter the username :");
+                username = Main.getUsername();
+                System.out.println("Please enter the password :");
+                password = Main.getPassword();
+                System.out.println("Plase re-enter your password: ");
+                String rePassword = Main.getPassword();
+                if (!password.equals(rePassword)) {
+                    System.out.println("Password are not equal !");
+                    System.exit(-1);
+                }
+                Setup.setup(username, password);   // this  method creates a javakeystoretool and a certificate
+                System.out.println("Setup ready !");
+            } else {
+
+                if (args[0].startsWith("-ip=")) {
+                    System.out.println("Please enter the username :");
+                    username = Main.getUsername();
+                    System.out.println("Please enter the password :");
+                    password = Main.getPassword();
+                    String ip = args[0].substring(4);
+
+                    //TODO: inceputul unui apel, instantierea clasei pentru apeluri
+                } else
+                    Main.Help();
+            }
+
         }
 
 
     }
 
+    /**
+     * This method gets the username from user
+     *
+     * @return a string which contains the username
+     */
+    public static String getUsername() {
+        String user = "";
 
-    public static byte[] readMessage(Records client) {
+
         try {
-            client.openPickupLine();
-            byte[] dataCaptured = client.captureAudioData();
-            return dataCaptured;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
+            StringBuilder string = new StringBuilder();
+            int ascii = System.in.read();
 
-    public static void playMessage(Playback client, byte[] dataToPlay) {
-        try {
-            client.openLineForPlayback();
-            client.play(dataToPlay);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+            while ((ascii != '\n') && (ascii != 13)) {
+                string.append((char) ascii);
+                ascii = System.in.read();
+            }
+            user = string.toString();
+        } catch (IOException ex) {
+            System.out.println("Error occured in reading the username !");
+            System.exit(-1);
 
-
-    public static void sendData(byte[] dataToSend) {
-        try {
-            CallerConnection ex = new CallerConnection("127.0.0.1", 8012);
-            ex.Open();
-            ex.Write(dataToSend);
-        } catch (Exception e) {
-            e.printStackTrace();
+        } finally {
+            return user;
         }
     }
 
-    public static byte[] receiveData() {
-
-        try {
-            CallerConnection ex = new CallerConnection("127.0.0.1", 8012);
-            ex.Open();
-            byte[] dataReceived = ex.Read();
-            return dataReceived;
-        } catch (Exception e) {
-            e.printStackTrace();
+    /**
+     * This method gets the password form user without typing to STDOUT the chars
+     *
+     * @return a String with the password
+     */
+    public static String getPassword() {
+        Console consola;
+        consola = System.console();
+        if (consola == null) {
+            System.out.println("Error occured  in reading the password ! ");
+            System.exit(-1);
         }
-        return null;
+        String password = new String(consola.readPassword());
+        return password;
+    }
+
+    /**
+     * This method prints the help text.
+     */
+    public static void Help() {
+        System.out.println();
+        System.out.println("For setup : java MainClassFile -setup ");
+        System.out.println("For calling an ip: java MainClassFile -ip=XX.XX.XX.XX ");
+        System.out.println("For waiting an call: java MainClassFile ");
     }
 }
